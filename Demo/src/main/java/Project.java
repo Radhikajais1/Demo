b0bacead610422efc399b60cd4cd7595375bca06
Radhika <Radhika@DESKTOP-S0V7M4T>
2022-09-27 12:19:47 +0530

    Hello World!

---
+++ Demo/src/main/java/Project.java
@@ -0,0 +1,15 @@
+
+public class Project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello!");
+		
+		System.out.println("Welcome to the code");
+		
+        
+
+	}
+
+}
