84b4e87700bdc2026be2856e0efda15e069009db
Radhika <Radhika@DESKTOP-S0V7M4T>
2022-09-27 12:27:58 +0530

    first commit

---
+++ Primenum/src/Primenum.java
@@ -0,0 +1,24 @@
+
+public class Primenum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int n=100; n<=200; n++) {
+			int temp=0;
+			for (int i=2; i<=n-1;i++) {
+				if (n%i==0) {
+					temp=temp+1;
+				}
+			}
+			if(temp==0) {
+				System.out.println(n);
+			}
+		}
+
+	}
+
+
+	}
+
+
